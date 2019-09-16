98f4b05a710ef0fb3031718751744b9e1ff8fd53
SunWeiSheng <sunweisheng@live.cn>
2019-09-16 17:09:16 +0800

    create project

---
+++ spring-cloud-consul-client/src/main/java/com/bluersw/consul/client/SpringCloudConsulClientApplication.java
@@ -0,0 +1,13 @@
+package com.bluersw.consul.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsulClientApplication.class, args);
+	}
+
+}
